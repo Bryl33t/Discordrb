fc67fe05459f149d44e8d3e959fd3a0881a4a6ac
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-13 18:09:56 +0200

    Update Channel.rb

--- Channel.rb
+++ Channel.rb
@@ -2,7 +2,7 @@ ENV["DISCORDRB_NONACL"] = "1" # initialise environnment variable for windows
 
 require 'discordrb' # import discordrb module
 
-client = Discordrb::Commands::CommandBot.new token: "YOUR_TOKEN", prefix: "./" # instance of CommandBot object token: "token", prefix: ./
+client = Discordrb::Commands::CommandBot.new token: "", prefix: "./" # instance of CommandBot object token: "token", prefix: ./
 
 puts "Bot started :)"
 
@@ -17,5 +17,14 @@ client.command :clear do |event_clear, *args|
   else
     event_clear.respond("No permission :x:")
   end
+end
+
+client.command :testChannel do |event_channel|
+  event_channel.respond "Channel name: %s" % event_channel.channel.name
+  event_channel.respond "NSFW: %s" % event_channel.channel.nsfw?
+  event_channel.respond((event_channel.channel.owner_id != nil) ? "OwnerID: %s" % event_channel.channel.owner_id : "OwnerID: nil")
+  event_channel.respond "Parent ID: %d " % event_channel.channel.parent_id
+  event_channel.respond "Channel position: %d" % event_channel.channel.position
+  event_channel.respond "Rate_limit per user %d" % event_channel.channel.rate_limit_per_user
 end
 client.run # connect to discord
