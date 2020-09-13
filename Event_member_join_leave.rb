6a1c5f256e36ab4335587486fc81974f23bab8a1
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-13 12:11:41 +0200

    Create Event_member_join_leave.rb

---
+++ Event_member_join_leave.rb
@@ -0,0 +1,29 @@
+ENV["DISCORDRB_NONACL"] = "1" # initialise environnment variable for windows
+
+require 'discordrb' # import discordrb module
+
+client = Discordrb::Commands::CommandBot.new token: "NzU0NDEyNTA4MTY0MzkwOTQy.X10XZg.aLH8vYwxgkwUor2lr94Fdw9C5MM", prefix: "./" # instance of CommandBot object token: "token", prefix: ./
+
+puts "Bot started :)"
+
+=begin
+  the function member_join require a block event
+  event is raised when member join
+=end
+client.member_join do |event_user_join|
+  event_user_join.send_message(754453848294424576, "%s vient de rejoindre le serveur" % event_user_join.user.name)
+end
+
+=begin
+  function member_leave require block event
+  event is raised when member leave server
+
+  [send_message]
+    id_channel
+    content
+=end
+client.member_leave do |event_user_leave|
+  event_user_leave.send_message(754453848294424576, "%s vient de quitter le serveur" % event_user_leave.user.name)
+end
+
+client.run # connect to discord
