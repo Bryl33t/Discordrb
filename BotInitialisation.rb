97b87a0a2319720275701a3145165d881ccade2a
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-13 11:50:48 +0200

    Update BotInitialisation.rb

--- BotInitialisation.rb
+++ BotInitialisation.rb
@@ -2,7 +2,7 @@ ENV["DISCORDRB_NONACL"] = "1" # initialise environnment variable for windows
 
 require 'discordrb' # import discordrb module
 
-client = Discordrb::Commands::CommandBot.new token: "NzU0NDEyNTA4MTY0MzkwOTQy.X10XZg.HpP9faB3nxcvregUDeXo_HqxBPg", prefix: "./" # instance of CommandBot object token: "token", prefix: ./
+client = Discordrb::Commands::CommandBot.new token: "YOUR_TOKEN", prefix: "PREFIX" # instance of CommandBot object token: "token", prefix: ./
 
 puts "Bot started :)"
 
